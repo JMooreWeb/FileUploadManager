1b55f67425f7de914e39d9aab9401edcf3f71188
James Moore <jmooreweb@outlook.com>
2015-09-09 11:36:09 -0400

    Inital commit

---
+++ FileUploadManager/Controllers/UploadController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUploadManager.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Upload/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Upload/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Upload/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Upload/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Upload/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Upload/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Upload/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FileUploadManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FileUploadManager.MvcApplication" Language="C#" %>
